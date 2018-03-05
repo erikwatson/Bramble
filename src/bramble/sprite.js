b98b77bead086b2092d13ef035bdec0b6e6c1d07
Erik Watson <championchap@gmail.com>
2018-03-05 19:26:46 +0000

    code style

--- src/bramble/sprite.js
+++ src/bramble/sprite.js
@@ -1,11 +1,6 @@
-function create (
-  x = 0,
-  y = 0,
-  width = 0,
-  height = 0,
-  texture = null,
-  color = '#ffffff'
-) {
+function create
+  (x = 0, y = 0, width = 0, height = 0, texture = null, color = '#ffffff')
+{
   return { x, y, width, height, texture }
 }
 
