c4e20e80bb3282cb67c2de7754f26004c86c3004
Erik Watson <championchap@gmail.com>
2021-03-27 05:57:19 +0000

    adding text tests

--- test/tests.js
+++ test/tests.js
@@ -101,6 +101,20 @@ test(
 )
 
 // text testing
+test('text', 'should say hello', gfx => {
+  gfx.clear('white')
+  gfx.text({ x: 22, y: 40 }, 'Hello')
+})
+
+test('text colour', 'should say hello in red', gfx => {
+  gfx.clear('black')
+  gfx.text({ x: 22, y: 40 }, 'Hello', 'red')
+})
+
+test('text font', 'should say hello in a serif font', gfx => {
+  gfx.clear('white')
+  gfx.text({ x: 22, y: 40 }, 'Hello', 'black', '16pt serif')
+})
 
 // image testing
 
