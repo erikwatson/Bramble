a6656e3e4f1ddfebdab44d99cbc19ba22245ea4a
Erik Watson <championchap@gmail.com>
2024-06-06 20:44:49 +0100

    Added a clamp function

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -6,6 +6,17 @@ export function toDegrees(radians: number): number {
   return radians * (180 / Math.PI)
 }
 
+export function clamp(value: number, min: number, max: number) {
+  if (min > max) {
+    console.error(`clamp: the minimum (${min}) and maximum (${max}) have been passed in the wrong order`)
+    return value
+  }
+
+  if (value < min) { return min }
+  if (value > max) { return max }
+  return value
+}
+
 export default {
   toRadians,
   toDegrees
